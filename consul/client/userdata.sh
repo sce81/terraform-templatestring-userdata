047a2315460e407be5bab6a15b5eebcd229c67f1
Simon Elliott <simon.elliott@hashicorp.com>
2025-07-08 18:29:01 +0100

    develop -added client userdata

--- consul/client/userdata.sh
+++ consul/client/userdata.sh
@@ -24,6 +24,7 @@ rm -f /opt/consul/config/tls.json
 
 # Startup
 
+sudo chown -R consul:consul /opt/consul
 sudo bash /opt/consul/tls/update-certificate-store.sh --cert-file-path /opt/consul/tls/client-cert.pem
 
 /opt/consul/bin/run-consul.sh --client --cluster-tag-key "Name" --cluster-tag-value $CLUSTER_TAG_VALUE --datacenter $DATACENTER --enable-gossip-encryption --gossip-encryption-key "$GOSSIP_ENCRYPTION_KEY" --enable-rpc-encryption --ca-path "/opt/consul/tls/ca.pem" --cert-file-path "/opt/consul/tls/client-cert.pem" --key-file-path "/opt/consul/tls/client-key.pem"
