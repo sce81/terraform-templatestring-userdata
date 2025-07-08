03329438f37983ba8ff7e20a62ed76017791c77f
Simon Elliott <simon.elliott@hashicorp.com>
2025-07-08 15:48:57 +0100

    develop -added client userdata

--- consul/client/userdata.sh
+++ consul/client/userdata.sh
@@ -4,14 +4,12 @@ NAME=${NAME}
 ROLE=${ROLE}
 DATACENTER=${CLUSTER_TAG_VALUE}
 
-CONSUL_CERT=$(aws ssm get-parameter --name /${CLUSTER_TAG_VALUE}/certificates --with-decryption | jq '.Parameter.Value')
-CONSUL_KEY=$(aws ssm get-parameter --name /${CLUSTER_TAG_VALUE}/keys --with-decryption | jq '.Parameter.Value')
+CONSUL_CERT=$(aws ssm get-parameter --name /${CLUSTER_TAG_VALUE}/client-certificates --with-decryption | jq '.Parameter.Value')
 CONSUL_LICENSE=$(aws ssm get-parameter --name /${CLUSTER_TAG_VALUE}/license --with-decryption | jq -r '.Parameter.Value')
 GOSSIP_ENCRYPTION_KEY=$(aws ssm get-parameter --name /${CLUSTER_TAG_VALUE}/gossip-key --with-decryption | jq -r '.Parameter.Value')
 
 
-echo $CONSUL_CERT > /opt/consul/tls/consul-agent-ca.pem
-echo $CONSUL_KEY > /opt/consul/tls/consul-agent-ca-key.pem
+echo $CONSUL_CERT > /opt/consul/tls/client-bundle.pem
 echo $CONSUL_LICENSE > /opt/consul/config/license.hclic
 export CONSUL_LICENSE_PATH=/opt/consul/config/license.hclic
 
@@ -21,4 +19,4 @@ rm -f /opt/consul/config/tls.json
 
 # Startup
 
-/opt/consul/bin/run-consul.sh --cluster-tag-key "Name" --cluster-tag-value "${CLUSTER_TAG_VALUE}" --datacenter $DATACENTER --enable-gossip-encryption --gossip-encryption-key "$GOSSIP_ENCRYPTION_KEY" --enable-rpc-encryption --cert-file-path "/opt/consul/tls/consul-agent-ca.pem" --key-file-path "/opt/consul/tls/consul-agent-ca-key.pem"
+/opt/consul/bin/run-consul.sh --client --cluster-tag-key "Name" --cluster-tag-value "${CLUSTER_TAG_VALUE}" --datacenter $DATACENTER --enable-gossip-encryption --gossip-encryption-key "$GOSSIP_ENCRYPTION_KEY" --enable-rpc-encryption --ca-path "/opt/consul/tls/client-bundle.pem" --cert-file-path "/opt/consul/tls/client-bundle.pem" --key-file-path "/opt/consul/tls/client-bundle.pem"
